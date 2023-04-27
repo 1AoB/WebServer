979e2deb6285c514eac9470d25958bc296697dc0
1AoB <78208268+1AoB@users.noreply.github.com>
2023-04-27 22:45:16 +0800

    Create poll.c

---
+++ IO多路复用_学习代码/poll.c
@@ -0,0 +1,89 @@
+#include <stdio.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+#include <poll.h>
+
+
+int main() {
+
+    // 创建socket
+    int lfd = socket(PF_INET, SOCK_STREAM, 0);
+    struct sockaddr_in saddr;
+    saddr.sin_port = htons(9999);
+    saddr.sin_family = AF_INET;
+    saddr.sin_addr.s_addr = INADDR_ANY;
+
+    // 绑定
+    bind(lfd, (struct sockaddr *)&saddr, sizeof(saddr));
+
+    // 监听
+    listen(lfd, 8);
+
+    /***poll代码(poll与select类似，但也是select的升级版，poll函数调用，修改的是struct pollfd的第三个参数，所以就用不着临时变量了)***/
+    // 初始化检测的文件描述符数组
+    struct pollfd fds[1024];
+    for(int i = 0; i < 1024; i++) {
+        fds[i].fd = -1;//表示该文件描述符暂时不可用
+        fds[i].events = POLLIN;//表示读事件
+    }
+    fds[0].fd = lfd;//将监听套接字 lfd 加入到 poll 的文件描述符集合中，以便在 poll 调用时检测是否有新的客户端连接请求到来。
+    int nfds = 0;//初始化 nfds 变量，表示当前集合中最大的文件描述符索引。在这里，由于集合中只有一个监听套接字 lfd，因此初始值为 0。
+
+    while(1) {
+
+        // 调用poll系统函数，让内核帮检测哪些文件描述符有数据
+        int ret = poll(fds, nfds + 1, -1);//poll这行函数会修改第三个参数revents
+        if(ret == -1) {
+            perror("poll");
+            exit(-1);
+        } else if(ret == 0) {
+            continue;
+        } else if(ret > 0) {
+            // 说明检测到了有文件描述符的对应的缓冲区的数据发生了改变
+            if(fds[0].revents & POLLIN) {
+                // 表示有新的客户端连接进来了
+                struct sockaddr_in cliaddr;
+                int len = sizeof(cliaddr);
+                int cfd = accept(lfd, (struct sockaddr *)&cliaddr, &len);
+
+                // 将新的文件描述符加入到集合中
+                for(int i = 1; i < 1024; i++) {
+                    if(fds[i].fd == -1) {
+                        fds[i].fd = cfd;
+                        fds[i].events = POLLIN;
+                        break;
+                    }
+                }
+
+                // 更新最大的文件描述符的索引
+                nfds = nfds > cfd ? nfds : cfd;
+            }
+
+            for(int i = 1; i <= nfds; i++) {
+                if(fds[i].revents & POLLIN) {
+                    // 说明这个文件描述符对应的客户端发来了数据
+                    char buf[1024] = {0};
+                    int len = read(fds[i].fd, buf, sizeof(buf));
+                    if(len == -1) {
+                        perror("read");
+                        exit(-1);
+                    } else if(len == 0) {
+                        printf("client closed...\n");
+                        close(fds[i].fd);
+                        fds[i].fd = -1;
+                    } else if(len > 0) {
+                        printf("read buf = %s\n", buf);
+                        write(fds[i].fd, buf, strlen(buf) + 1);
+                    }
+                }
+            }
+
+        }
+
+    }
+    close(lfd);
+    return 0;
+}
+
