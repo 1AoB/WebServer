db4b936975732fcbaef6a92ba85dc9128a84ad68
1AoB <78208268+1AoB@users.noreply.github.com>
2023-04-27 22:43:01 +0800

    Create select.c

---
+++ IO多路复用_学习代码/select.c
@@ -0,0 +1,85 @@
+#include <stdio.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/select.h>
+
+int main() {
+
+    // 创建socket
+    int lfd = socket(PF_INET, SOCK_STREAM, 0);
+    struct sockaddr_in saddr;
+    saddr.sin_port = htons(9999);
+    saddr.sin_family = AF_INET;
+    saddr.sin_addr.s_addr = INADDR_ANY;
+
+    // 绑定
+    bind(lfd, (struct sockaddr *)&saddr, sizeof(saddr));
+
+    // 监听
+    listen(lfd, 8);
+
+    /***select代码***/
+    // 创建一个fd_set的集合，存放的是需要检测的文件描述符
+    fd_set rdset, tmp;
+    FD_ZERO(&rdset);
+    //FD_SET将其对应位设置为1
+    FD_SET(lfd, &rdset);//监听的文件描述符也要加入进入，当监听的文件描述符有数据，说明有客户端连接进来了；
+    //当客户端的文件描述符有数据，说明有客户端给服务器发送数据来了
+    int maxfd = lfd;
+
+    while(1) {
+
+        //select需要一个临时变量
+        tmp = rdset;//如果不拷贝rdset集合，则可能导致程序无法正确响应新的客户端连接请求。
+
+        // 调用select系统函数，让内核帮检测哪些文件描述符有数据
+        //为什么要用临时变量tmp保存rdset的原因：select会修改fd_set集合，select就是监测tmp中位图的1的文件描述符是否有读写操作，如果有就设置为1，如果没有就设置为0
+        int ret = select(maxfd + 1, &tmp, NULL, NULL, NULL);
+        if(ret == -1) {
+            perror("select");
+            exit(-1);
+        } else if(ret == 0) {
+            continue;
+        } else if(ret > 0) {
+            // 说明检测到了有文件描述符的对应的缓冲区的数据发生了改变
+            if(FD_ISSET(lfd, &tmp)) {
+                // 表示有新的客户端连接进来了
+                struct sockaddr_in cliaddr;
+                int len = sizeof(cliaddr);
+                int cfd = accept(lfd, (struct sockaddr *)&cliaddr, &len);
+
+                // 将新的文件描述符加入到集合中
+                FD_SET(cfd, &rdset);
+
+                // 更新最大的文件描述符
+                maxfd = maxfd > cfd ? maxfd : cfd;
+            }
+
+            for(int i = lfd + 1; i <= maxfd; i++) {
+                if(FD_ISSET(i, &tmp)) {
+                    // 说明这个文件描述符对应的客户端发来了数据
+                    char buf[1024] = {0};
+                    int len = read(i, buf, sizeof(buf));
+                    if(len == -1) {
+                        perror("read");
+                        exit(-1);
+                    } else if(len == 0) {
+                        printf("client closed...\n");
+                        close(i);
+                        FD_CLR(i, &rdset);//清空一下
+                    } else if(len > 0) {
+                        printf("read buf = %s\n", buf);
+                        write(i, buf, strlen(buf) + 1);
+                    }
+                }
+            }
+
+        }
+
+    }
+    close(lfd);
+    return 0;
+}
+
