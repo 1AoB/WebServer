d46409a444f77d1ab8a0f2032ba7d208b395d0c4
1AoB <78208268+1AoB@users.noreply.github.com>
2023-05-03 18:36:57 +0800

    Update main.cpp

--- code/main.cpp
+++ code/main.cpp
@@ -12,7 +12,7 @@ int main()
     // daemon(1, 0);
 
     // http://192.168.8.128:8989
-    WebServer server(
+    WebServer server(//将WebServer封装成一个类
         8989 /*1316*/, 3, 60000, false,    /* 端口 ET模式 timeoutMs 优雅退出  */
         3306, "root", "root", "webserver", /* Mysql配置 */
         12, 6, true, 1, 1024);             /* 连接池数量 线程池数量 日志开关 日志等级 日志异步队列容量 */
