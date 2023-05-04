a6952736514b0edd27a4d22c726d16ed03decee5
1AoB <78208268+1AoB@users.noreply.github.com>
2023-05-04 13:10:06 +0800

    Update main.cpp

--- code/main.cpp
+++ code/main.cpp
@@ -16,5 +16,5 @@ int main()
         8989 /*1316*/, 3, 60000, false,    /* 端口 ET模式 timeoutMs 优雅退出  */
         3306, "root", "root", "webserver", /* Mysql配置 */
         12, 6, true, 1, 1024);             /* 连接池数量 线程池数量 日志开关 日志等级 日志异步队列容量 */
-    server.Start();
+    server.Start();                        // 开启服务端
 }
